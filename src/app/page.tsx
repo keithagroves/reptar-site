2291e924bd6f7a53584537b91e378fa44ee6ff2f
kanyecat <89665006+kanyecat@users.noreply.github.com>
2024-06-20 19:48:41 -0700

    nav, hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,26 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
+    <>
+    <div className={styles.navbar}>
+        <div className="logo"><a>reptar.ai</a></div>
+          <ul>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+    </div>
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>reptar.ai</h1>
+        <h1>Create Business Avatar</h1>
+        <div className={styles.input}>
+          <input type="text" placeholder="Insert website link" className={styles.userinput}></input>
+          <button className={styles.generatebutton}>Generate Avatar</button>
+        </div>
       </main>
       <footer className={styles.footer}>
   
       </footer>
     </div>
+    </>
   );
 }
